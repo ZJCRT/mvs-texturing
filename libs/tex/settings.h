89fec5e38db1e266ead083018c149f624cd27ab7
Johannes Koehler <johannes.koehler@zeiss.com>
2022-01-20 14:44:51 +0100

    Added command line argument for filling all holes in the texture atlas
    Signed-off-by: Johannes Koehler <johannes.koehler@zeiss.com>

--- libs/tex/settings.h
+++ libs/tex/settings.h
@@ -92,6 +92,7 @@ struct Settings {
     bool local_seam_leveling = true;
     bool hole_filling = true;
     bool keep_unseen_faces = false;
+    bool fill_all_holes = false;
 };
 
 TEX_NAMESPACE_END
